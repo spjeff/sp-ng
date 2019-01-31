cdf3b454c22ee83fb38c36c3aaca0915bff1bffa
Sumit Agrawal <sumit.r.agrawal@hotmail.com>
2019-01-31 02:45:45 +0000

    typeLibCheck flag, service registration in app.module and removal of httpclient service injection

--- src/app/spservice.service.ts
+++ src/app/spservice.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { Observable, from } from 'rxjs';
 
 import { sp, Web } from '@pnp/sp';
@@ -9,7 +8,6 @@ const web = new Web(environment.web);
 
 @Injectable()
 export class SPServiceService {
-  constructor(private http: Http) {}
 
   getWebTitle(): Observable<any> {
     return from(web.get());
